24fbee8beeaf1aa529295f393dd12d4e00b7f36a
xiomi <xiomara.torresg01@gmail.com>
2025-06-17 19:53:20 +0200

    Configuration database.

---
+++ back/app/core/config.py
@@ -0,0 +1,30 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file="../.env", 
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+    
+    APP_NAME: str = "API Prueba Tecnica DIVAIN"
+    API_V1: str = "/api/v1"
+    
+    POSTGRES_HOST:str
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    @property
+    def DATABAS_URI(self):
+        return (
+            f"postgresql+asyncpg://"
+            f"{self.POSTGRES_USER}:"
+            f"{self.POSTGRES_PASSWORD}@"
+            f"{self.POSTGRES_HOST}:"
+            f"{self.POSTGRES_PORT}/"
+            f"{self.POSTGRES_DB}"
+    )
+
+settings = Settings()
