28adc6195315c7b963c84997f5cfd408ef99e0f5
xiomi <xiomara.torresg01@gmail.com>
2025-06-22 14:19:22 +0200

    Fix SessionDep.

--- back/app/core/db.py
+++ back/app/core/db.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase, Session
+from sqlalchemy.orm import DeclarativeBase
 from fastapi import Depends
 
 from app.core.config import settings
@@ -9,7 +9,7 @@ from app.core.config import settings
 engine = create_async_engine(settings.DATABASE_URI, echo=True)
 
 AsyncSessionLocal = async_sessionmaker(
-    engine, class_=AsyncSession, expire_on_commit=True
+    engine, class_=AsyncSession, expire_on_commit=False
 )
 
 class Base(DeclarativeBase):
@@ -23,4 +23,4 @@ async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-SessionDep = Annotated[Session, Depends(get_session)]
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
