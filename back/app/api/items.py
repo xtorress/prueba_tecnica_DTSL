f951b7cb6ab0da424b06b613bba9da8696bbbc50
xiomi <xiomara.torresg01@gmail.com>
2025-06-24 20:58:22 +0200

    Add function to create item.

--- back/app/api/items.py
+++ back/app/api/items.py
@@ -4,32 +4,43 @@ from fastapi import APIRouter,  HTTPException
 from app.core.db import SessionDep
 from app.schemas import ItemBase, StockHistoryBase, \
                         StockUpdateRequest
-from app.crud.item import get_items, get_histories_stock, get_item_by_id, update_item_stock
+from app.crud import item as crud
+from app.crud.item import DuplicateItemError
 
 router = APIRouter(prefix="/items", tags=['items'])
 
 @router.get("/", response_model=List[ItemBase])
 async def list_items(session: SessionDep):
-    items = await get_items(session)
+    items = await crud.get_items(session)
     return items
 
 @router.get("/history", response_model=List[StockHistoryBase])
 async def list_histories(session: SessionDep):
-    histories = await get_histories_stock(session)
+    histories = await crud.get_histories_stock(session)
     return histories
 
-@router.patch("/{item_id}/stock")
+@router.post("/", status_code=201)
+async def create_item(session: SessionDep, item_in: ItemBase):
+    try:
+        item = await crud.create_item(session, item_in)
+    except DuplicateItemError as e:
+        raise HTTPException(status_code=401, detail=f"Valor {e} ya existe.")
+    return {
+        "msg": "Item creado correctamente.",
+        "data": item
+    }
+
+@router.patch("/{item_id}/stock", status_code=200)
 async def update_stock(session: SessionDep, item_id: int, data:StockUpdateRequest):
     try:
-        item = await get_item_by_id(session, item_id)
+        item = await crud.get_item_by_id(session, item_id)
         if not item:
             raise HTTPException(status_code=404, detail="Item no encontrado.")
         if data.new_stock < 0:
             raise HTTPException(status_code=400, detail="El stock no puede ser negativo.")
-        movement = await update_item_stock(session, item, data.new_stock)
+        movement = await crud.update_item_stock(session, item, data.new_stock)
         # await session.refresh()
         return {
-            "status": 200,
             "msg":"Stock actualizado correctamente.",
             "data":movement
         }
@@ -41,4 +52,3 @@ async def update_stock(session: SessionDep, item_id: int, data:StockUpdateReques
             status_code=500,
             detail=f"Error al actualizar stock: {str(e)}"
         )
-
--- back/app/crud/item.py
+++ back/app/crud/item.py
@@ -2,7 +2,11 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import Item, StockHistory
-from app.schemas import ItemSchema
+from app.schemas import ItemSchema, ItemBase
+
+class DuplicateItemError(Exception):
+    def __init__(self, value: str):
+        self.value = value
 
 async def get_items(session: AsyncSession):
     stmt = select(Item)
@@ -13,6 +17,28 @@ async def get_item_by_id(session: AsyncSession, id: int):
     stmt = select(Item).where(Item.id == id)
     result = await session.execute(stmt)
     return result.scalar_one_or_none()
+
+async def get_item_by_sku_or_ean13(session: AsyncSession, ean13: str, sku: str):
+    stmt = select(Item).where((Item.ean13 == ean13 ) | (Item.sku == sku))
+    result = await session.execute(stmt)
+    return result.scalar_one_or_none()
+
+# async def get_item_by_ean13(session: AsyncSession, ean13: int):
+#     pass
+
+async def create_item(session, item: ItemBase):
+    exist = await get_item_by_sku_or_ean13(session, item.ean13, item.sku)
+    if exist:
+        if exist.ean13 == item.ean13:
+            raise DuplicateItemError("ean13")
+        elif exist.sku == item.sku:
+            raise DuplicateItemError("sku")
+    
+    new_item = Item(**item.model_dump())
+    session.add(new_item)
+    await session.commit()
+    await session.refresh(new_item)
+    return new_item
 
 async def update_item_stock(session: AsyncSession, item:ItemSchema, new_stock):
     """Update item stock and create a StockHistory."""
