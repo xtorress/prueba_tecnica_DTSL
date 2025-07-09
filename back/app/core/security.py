c90ddc7f139d4c5b0a489f353d17fe440ae5ff43
xiomi <xiomara.torresg01@gmail.com>
2025-07-09 18:20:12 +0200

    Add function to create access token

--- back/app/core/security.py
+++ back/app/core/security.py
@@ -1,10 +1,18 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
 from app.core.config import settings
+
+import jwt
 
 ALGORITHM = "HS256"
 
 pwd_context = CryptContext(schemes=["bcrypt"])
+
+def create_token(subject: str, expire_delta: timedelta) -> str:
+    expire = datetime.now(timezone.utc) + expire_delta
+    to_encoded = {"exp": expire, "sub": subject}
+    encoded_jwt = jwt.encode(to_encoded, settings.SECRET_KEY, ALGORITHM)
+    return encoded_jwt
 
 def get_password_hash(pwd: str) -> str:
     return pwd_context.hash(pwd)
